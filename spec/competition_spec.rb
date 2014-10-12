f3e8a6a2cd1fc19b43dcb2f7087de45fa147d130
winnab <winna.bridgewater@mammal.io>
2014-10-13 00:43:24 +0100

    Change symbol to array to match format of tut

--- spec/competition_spec.rb
+++ spec/competition_spec.rb
@@ -14,8 +14,15 @@ describe Competition do
   end
 
   context "having questions" do
-    before { competition.stub( :questions => :stub ) }
+    before { competition.stub( :questions => [ stub ] ) }
     subject { competition }
     it { should allow_teams_to_enter }
+  end
+
+  context "when started" do
+    it "is closed" do
+      competition.should_receive(:close)
+      competition.start
+    end
   end
 end
