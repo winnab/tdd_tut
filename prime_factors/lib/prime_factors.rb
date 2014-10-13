afd5bcc5e6b959b8c26000baa6e8b4ba0a44cf75
winnab <winna.bridgewater@mammal.io>
2014-10-13 02:52:56 +0100

    refactor while loop and var definitions

--- prime_factors/lib/prime_factors.rb
+++ prime_factors/lib/prime_factors.rb
@@ -1,10 +1,8 @@
 module PrimeFactors
   def self.of(n)
-    factors = []
-    divisor = 2
-    while n > 1
+    factors, divisor = [], 1
+    while n > 1 and divisor += 1
       factors << divisor and n/=divisor while n%divisor == 0
-      divisor += 1
     end
     factors
   end
