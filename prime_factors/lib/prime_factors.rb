c789dd1f25598e355ccbe42f2b6f4e2bce439e08
winnab <winna.bridgewater@mammal.io>
2014-10-13 02:47:42 +0100

    refactor logic that determines factors of n. iterate over values greater than 1 rather than having specific checks for each low prime.

--- prime_factors/lib/prime_factors.rb
+++ prime_factors/lib/prime_factors.rb
@@ -1,8 +1,10 @@
 module PrimeFactors
   def self.of(n)
     factors = []
-    if n > 1
-      factors << 2 and n/=2 while n%2 == 0
+    divisor = 2
+    while n > 1
+      factors << divisor and n/=divisor while n%divisor == 0
+      divisor += 1
     end
     factors << n if n > 1
     factors
