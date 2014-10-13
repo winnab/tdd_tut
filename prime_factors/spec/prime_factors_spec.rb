db7b9975225c0d11f2092c63d6db12e7b22f3518
winnab <winna.bridgewater@mammal.io>
2014-10-13 02:11:31 +0100

    pass a block and check that values equal all factors

--- prime_factors/spec/prime_factors_spec.rb
+++ prime_factors/spec/prime_factors_spec.rb
@@ -3,6 +3,14 @@ gem 'rspec'
 require_relative '../lib/prime_factors.rb'
 
 describe PrimeFactors do
+  [
+    [1, []]
+  ].each do |n, factors|
+    it "should factor #{n}" do
+      PrimeFactors.of(n).should == factors
+    end
+  end
+
   it "should factor 1" do
     PrimeFactors.of(1).should == []
   end
