6252a87786d6108ef2ddd38c93c75e918c4bd0c7
winnab <winna.bridgewater@mammal.io>
2014-10-13 02:39:39 +0100

    iterate over factors divisible by two until you get down to two

--- prime_factors/lib/prime_factors.rb
+++ prime_factors/lib/prime_factors.rb
@@ -2,7 +2,7 @@ module PrimeFactors
   def self.of(n)
     factors = []
     if n > 1
-      factors << 2 and n/=2 if n%2 == 0
+      factors << 2 and n/=2 while n%2 == 0
     end
     factors << n if n > 1
     factors
--- prime_factors/spec/prime_factors_spec.rb
+++ prime_factors/spec/prime_factors_spec.rb
@@ -9,8 +9,9 @@ describe PrimeFactors do
     ['3', [3]],
     ['4', [2, 2]],
     ['5', [5]],
-    ['6', [2, 3]]
-    ['7', [7]]
+    ['6', [2, 3]],
+    ['7', [7]],
+    ['8', [2]* 3]
   ].each do |expression, factors|
     it "should factor #{expression}" do
       number = eval expression
