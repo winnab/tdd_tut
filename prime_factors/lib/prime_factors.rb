8101d03769d5e0cc1b8d1b163d2a30817152207c
winnab <winna.bridgewater@mammal.io>
2014-10-13 03:19:01 +0100

    refactor for testing much larger numbers

--- prime_factors/lib/prime_factors.rb
+++ prime_factors/lib/prime_factors.rb
@@ -3,6 +3,8 @@ module PrimeFactors
     factors, divisor = [], 1
     while n > 1 and divisor += 1
       factors << divisor and n/=divisor while n%divisor == 0
+      divisor = n-1 if divisor > Math.sqrt(n)
+      # every composite number has a prime factor less than or equal to its square root
     end
     factors
   end
