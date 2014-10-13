f3d77ea5ca01fd6ee188912c0df9a5d0046535bc
winnab <winna.bridgewater@mammal.io>
2014-10-13 02:19:19 +0100

    remove checks for specific factors now that refactored block passes

--- prime_factors/spec/prime_factors_spec.rb
+++ prime_factors/spec/prime_factors_spec.rb
@@ -13,12 +13,4 @@ describe PrimeFactors do
     end
   end
 
-  it "should factor 1" do
-    PrimeFactors.of(1).should == []
-  end
-
-  it "should factor 2" do
-    PrimeFactors.of(2).should == [2]
-  end
-
 end
